aedb90ff78043393a87f852ea19b257d6e67b049
root <2917073217@qq.com>
2017-02-09 10:40:23 +0800

    3.2.php

---
+++ 3.2.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>系统内置常量</title>
+</head>
+<body>
+<?php
+	echo (__FILE__)."<BR>";
+	echo (__LINE__)."<br>";
+	echo PHP_VERSION."<BR>";
+	echo PHP_OS;
+?>
+
+</body>
+
+</html>
