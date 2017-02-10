95b39e538400690c959d2b26a60259d3505f6a27
root <2917073217@qq.com>
2017-02-10 14:48:13 +0800

    all

---
+++ 3.8.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>bian liang de lei xing</title>
+</head>
+<body>
+<?php
+$int1=2012;
+$int2=01223;
+$int3=0x1223;
+echo "shu chu zheng shu lei xing de zhi:"."<br>";
+echo $int1."  ".$int2."  ".$int3."<br>";
+$float1=54.66;
+echo "float lei xing de zhi :".$float1."<br>";
+echo "shu chu boolean zhi:".(boolean)($int1)."<br>";
+$string="this is string";
+echo $string;
+?>
+</body>
+</html>
